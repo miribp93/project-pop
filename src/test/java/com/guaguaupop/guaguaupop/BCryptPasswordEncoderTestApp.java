429926fd3280151161cb975e80c2e53c1e3351ce
miribp93 <miribp93@hotmail.com>
2024-11-03 18:56:16 +0100

    compila java y angular

--- src/test/java/com/guaguaupop/guaguaupop/BCryptPasswordEncoderTestApp.java
+++ src/test/java/com/guaguaupop/guaguaupop/BCryptPasswordEncoderTestApp.java
@@ -1,11 +1,9 @@
 package com.guaguaupop.guaguaupop;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
 
 public class BCryptPasswordEncoderTestApp  {
     public static void main(String[] args) {
